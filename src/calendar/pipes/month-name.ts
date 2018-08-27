10fb35c49d9b267a36d9a8a1c652c66c003235c6
clemouuuche <35827169+clemouuuche@users.noreply.github.com>
2018-08-27 03:40:32 -0400

    Update month-name.ts

--- src/calendar/pipes/month-name.ts
+++ src/calendar/pipes/month-name.ts
@@ -1,12 +1,14 @@
 import {Pipe} from '@angular/core';
 import * as moment from 'moment';
+import 'moment/locale/fr';
+
 
 @Pipe({
   name: 'monthName'
 })
 export class monthName {
   transform(value, args) {
-    var lang = moment().locale(args);
+    var lang = moment().locale('fr);
     return lang.localeData().months(moment([2012, value - 1]));
   }
 }
